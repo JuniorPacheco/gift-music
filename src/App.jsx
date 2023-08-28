d39db1d90b15799f344512809bbc56a8198baa54
JuniorPacheco <junior.pacheco.356@gmail.com>
2023-08-27 23:38:53 -0500

    configure routes and create all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import {
+  ArtistDetail,
+  Home,
+  Login,
+  PlaylistDetail,
+  PlaylistPublic,
+  Playlists,
+  Register,
+  TrackDetail,
+} from "./pages";
 
 function App() {
-
   return (
-    <>
-      <h2 className='text-2xl font-bold'>Hola mundo!</h2>
-    </>
-  )
+    <Routes>
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/register" element={<Register />} />
+
+      <Route path="/" element={<Home />} />
+      <Route path="/artist/:id" element={<ArtistDetail />} />
+      <Route path="/track/:id" element={<TrackDetail />} />
+      <Route path="/playlists" element={<Playlists />} />
+      <Route path="/playlist/:id" element={<PlaylistDetail />} />
+
+      <Route path="/playlist/public/:id" element={<PlaylistPublic />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export { default as ArtistDetail } from './ArtistDetail';
+export { default as Home } from './Home';
+export { default as Login } from './Login';
+export { default as PlaylistDetail } from './PlaylistDetail';
+export { default as PlaylistPublic } from './PlaylistPublic';
+export { default as Playlists } from './Playlists';
+export { default as Register } from './Register';
+export { default as TrackDetail } from './TrackDetail';
