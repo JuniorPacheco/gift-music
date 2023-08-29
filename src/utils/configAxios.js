c63212afa921b7d834e0b9c55a59951cf2d72237
JuniorPacheco <junior.pacheco.356@gmail.com>
2023-08-29 00:24:22 -0500

    implement instance axios and intercep request to authorization

--- src/utils/configAxios.js
+++ src/utils/configAxios.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
 const axiosMusic = axios.create({
-  baseURL: "https://playlist-share-dev.fl0.io"
-})
+  baseURL: "https://playlist-share-dev.fl0.io",
+});
 
+axiosMusic.interceptors.request.use((config) => {
+  config.headers.Authorization = `JWT ${
+    localStorage.getItem("userInfo") &&
+    JSON.parse(localStorage.getItem("userInfo")).state.user.token
+  }`;
+  return config;
+});
 
-export {
-  axiosMusic
-}
+axiosMusic.de;
+export { axiosMusic };
