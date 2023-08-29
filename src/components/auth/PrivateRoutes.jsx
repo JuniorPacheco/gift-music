30596cf84a13f3cf4a63f2d0666c175354a3f2e8
JuniorPacheco <junior.pacheco.356@gmail.com>
2023-08-29 00:23:38 -0500

    Added principal layout to all private routes

--- src/components/auth/PrivateRoutes.jsx
+++ src/components/auth/PrivateRoutes.jsx
@@ -1,9 +1,15 @@
-import { Navigate, Outlet } from "react-router-dom"
-import { useUserStore } from "../../store/user"
+import { Navigate, Outlet } from "react-router-dom";
+import { useUserStore } from "../../store/user";
+import PrincipalLayout from "../layout/PrincipalLayout";
 
 const PrivateRoutes = () => {
-  const {token} = useUserStore(state => state.user)
-  if(token) return <Outlet />
-  return <Navigate to='/auth/login' />
-}
-export default PrivateRoutes
+  const { token } = useUserStore((state) => state.user);
+  if (token)
+    return (
+      <PrincipalLayout>
+        <Outlet />
+      </PrincipalLayout>
+    );
+  return <Navigate to="/auth/login" />;
+};
+export default PrivateRoutes;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,11 @@ export default {
     extend: {
       colors: {
         primary: "#FFE24B",
+        "yellow-p": "#EDD741"
       },
       backgroundImage: {
         secondary: "linear-gradient(145deg, #886AE2 45.75%, #A284F6 100%)",
+        "purple-gradient": "linear-gradient(135deg, rgba(61, 46, 149, 0.35) 0%, #3D2E95 100%)"
       },
     },
     fontFamily: {
